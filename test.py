a8d3fcbd2a41ef9083f8715bea274996718200be
stevenyen510 <stevenyen510@gmail.com>
2017-05-02 00:19:09 -0700

    Added random AI to test.py

--- test.py
+++ test.py
@@ -1,3 +1,30 @@
-A=1+2
+import Connect4Interface
+import random
 
-print('test')
+#A=1+2
+
+#print('test')
+
+
+
+
+class GameWithAI(Connect4Interface.Connect4Game):
+    """This class inherits the Connect4Game class in Connect4Interface.py
+    it simply overrides the p2_next_move()"""
+                 
+    def p2_next_move(self,currentBoard):
+        """This overrides the p2_next_move() function in the original class
+        to impelement an AI. The "AI" here simply picks a random move between
+        column 0 and 6"""
+        print "Move by random AI"
+        print
+        column = random.randint(0,6)
+        return column
+        
+#print "New Game with AI"        
+#gameWithAI_1 = GameWithAI()
+#gameWithAI_1.run_game()
+
+        
+        
+        
